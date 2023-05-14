b3ca23724f871dc1ef6c63eb4f3b163515f1062a
Fathul Irfaan Abdillah <fathdev@pop-os.localdomain>
2023-05-15 05:52:04 +0700

    Optimized image and added robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), sitemap()],
+  experimental: {
+    assets: true
+  }
 });
